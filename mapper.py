c66f8c213b65b03bf148c37c1ea19645eb74f737
rhamilton1008 <97925124+rhamilton1008@users.noreply.github.com>
2024-06-11 17:46:47 -0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -5,7 +5,6 @@ import re
 
 for line in sys.stdin:
     line = line.strip()
-    # Find all contiguous strings of letters using regex
     words = re.findall(r'[a-zA-Z]+', line)
     for word in words:
         print('%s\t%s' % (word, 1))
